d69ef20b564e113e366feddbe56e5a8b38406fa8
Nelsonrsl.com <nelsonribera1@gmail.com>
2023-05-23 12:16:48 -0400

    first commit

---
+++ index.php
@@ -0,0 +1,23 @@
+<!DOCTYPE html>
+<html lang="es">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Inicio | Autosud LTDA</title>
+</head>
+<body>
+    <main>
+        <header>
+            <nav>
+                <ul class="flex-item-1">
+                    <li>Autosud LTDA</li>
+                </ul>
+                <ul class="flex-item-1">
+                    <li>Autosud LTDA</li>
+                </ul>
+            </nav>
+        </header>
+    </main>
+</body>
+</html>
