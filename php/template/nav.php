dd61d7e7414d7f1a6e206b7991a29087c0bc14ee
Nelsonrsl.com <nelsonribera1@gmail.com>
2023-05-23 12:31:13 -0400

    nav

---
+++ php/template/nav.php
@@ -0,0 +1,10 @@
+<header>
+    <nav>
+        <ul class="flex-item-1">
+            <li><a href="/">Autosud LTDA</a></li>
+        </ul>
+        <ul class="flex-item-1">
+            <li><a href="/financiamiento.php">Financiamiento</a></li>
+        </ul>
+    </nav>
+</header>
